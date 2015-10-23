aa9fb5221160cc348a3d1fe8c030d99161c6377a
FrancescoBianchi <francesco.bianchi.os@gmail.com>
2015-10-23 21:28:25 +0100

    Added test for the controller

---
+++ test/integration/com/intercom/interview/guest/GuestControllerTest.groovy
@@ -0,0 +1,29 @@
+package com.intercom.interview.guest;
+
+import static org.junit.Assert.*;
+import grails.test.mixin.TestFor;
+
+import org.junit.Test;
+
+import spock.lang.Specification;
+
+@TestFor(GuestController)
+class GuestControllerTest extends Specification {
+
+    void "test controllers returns a non empty list of guests in the response"() {
+        when:
+        def model = controller.list()
+
+        then:
+        model.guests != null
+		!model.guests.isEmpty()
+		model.guests[0] instanceof Guest
+		model.guests[0].with {
+			name
+			userId
+			coordinate != null
+			coordinate.latitude > 0
+		}
+    }
+
+}
